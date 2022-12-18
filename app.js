c2e165cd86cbb390adb4615b728dca7a96fef457
Oguzcan Aygun <aygunoguzcan@outlook.com>
2022-12-18 15:17:22 +0300

    add views

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items=["Buy Food","Cook Food","Eat Food"];
+let items=["Buy Food","Cook Food","Eat Food"];
+let workItems=[];
 
 app.set('view engine', 'ejs');
 
@@ -12,11 +13,11 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
+  let today = new Date();
+  let currentDay = today.getDay();
   var day = "";
 
-  var options = {
+  let options = {
     weekday : "long",
     day : "numeric",
     month : "long"
@@ -27,10 +28,31 @@ app.get("/", function(req, res) {
 
 });
 app.post("/", function(req, res){
-  var item = req.body.newItem;
-  items.push(item);
 
-  res.redirect("/");
+  var item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle:"Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res) {
+  let item=req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function() {
